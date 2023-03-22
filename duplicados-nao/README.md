d4cf6a676670e01ccdd9053afb6df4a2a976e4cd
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2023-03-22 18:04:30 -0300

    feat: exercise no duplicates

---
+++ duplicados-nao/README.md
@@ -0,0 +1,12 @@
+## Duplicados não! :
+
+- Enunciado:
+
+  Escreva um código que encontre os elementos em comum que existem em dois arrays e imprima os nomes dos itens em comum! Exemplo:
+
+  ```
+  Array 1: Morango, Uva, Acerola, Manga
+  Array 2: Pêra, Caju, Morango, Acerola
+
+  Resultado: [Morango, Acerola]
+  ```
---
+++ duplicados-nao/src/App.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String[] ArrFruits1 = { "Morango", "Uva", "Acerola", "Manga" };
+        String[] ArrFruits2 = { "Pêra", "Caju", "Morango", "Acerola" };
+
+        List<String> duplicatedFruits = new ArrayList<>();
+
+        for (int i = 0; i < ArrFruits1.length; i++) {
+            for (int j = 0; j < ArrFruits1.length; j++) {
+                if (ArrFruits1[i].equals(ArrFruits2[j])) {
+                    duplicatedFruits.add(ArrFruits1[i]);
+                }
+            }
+        }
+       
+        System.out.println(duplicatedFruits);
+    }
+}
